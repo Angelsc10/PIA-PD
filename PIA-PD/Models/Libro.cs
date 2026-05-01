4e23cd007e7dd9fc10628e73700b446b8771e2a7
Angel Soto <sotoabgel@gmail.com>
2026-05-01 13:17:52 -0600

    (fix): You just can put 2 decimals in price and in edit window.

--- PIA-PD/Models/Libro.cs
+++ PIA-PD/Models/Libro.cs
@@ -16,15 +16,16 @@ namespace PIA_PD.Models
 
         public string? PortadaUrl { get; set; }
 
-        [Required(ErrorMessage = "El precio es obligatorio")]
-        [Range(0.01, 999999.99, ErrorMessage = "El precio debe ser mayor a 0")]
-        [Column(TypeName = "decimal(18,4)")]
-        public decimal Precio { get; set; }
-
         [Required(ErrorMessage = "La cantidad es obligatoria")]
         [Range(1, int.MaxValue, ErrorMessage = "La cantidad inicial debe ser al menos 1")]
         public int Stock { get; set; }
 
         public string Categoria { get; set; } = "Ficción";
+
+        [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "El precio solo puede tener hasta 2 decimales.")]
+        public decimal Precio { get; set; }
     }
 }
